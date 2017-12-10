045c1ef0b56ab209660a7719c6a1dd0ef5791d64
MimiMagusin <mimi_magusin@hotmail.com>
2017-12-10 19:08:06 +0100

    Set Up route PUT, need to fix test get one movie

--- test/routes.movies.test.js
+++ test/routes.movies.test.js
@@ -121,3 +121,52 @@ describe('POST /api/v1/movies', () => {
       });
   });
 });
+
+describe('PUT /api/v1/movies', () => {
+  it('should return the movie that was updated', (done) => {
+    knex('movies')
+      .select('*')
+      .then((movie) => {
+        const movieObject = movie[0];
+        chai.request(server)
+          .put(`/api/v1/movies/${movieObject.id}`)
+          .send({
+            rating: 9
+          })
+          .end((err, res) => {
+            should.not.exist(err);
+            res.status.should.equal(200);
+            res.type.should.equal('application/json');
+            res.body.status.should.eql('success');
+            // the JSON response body should have a
+            // key-value pair of {"data": 1 movie object}
+            res.body.data[0].should.include.keys(
+              'id', 'name', 'genre', 'rating', 'explicit'
+            );
+            // ensure the movie was in fact updated
+            const newMovieObject = res.body.data[0];
+            newMovieObject.rating.should.not.eql(movieObject.rating);
+            done();
+          });
+      });
+  });
+  it('should throw an error if the movie does not exist', (done) => {
+    chai.request(server)
+      .put('/api/v1/movies/9999999')
+      .send({
+        rating: 9
+      })
+      .end((err, res) => {
+        should.exist(err);
+        res.status.should.equal(404);
+        res.type.should.equal('application/json');
+        // the JSON response body should have a
+        // key-value pair of {"status": "error"}
+        res.body.status.should.eql('error');
+        // the JSON response body should have a
+        // key-value pair of {"message": "That movie does not exist."}
+        res.body.message.should.eql('That movie does not exist.');
+        done();
+      });
+    });
+});
